ab0d3d7b6a6b63e2625e3e2a5d3185e926c6159f
ifeanyi Moghalu <chrisamobi8@gmail.com>
2022-09-08 14:33:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * description: 'the program's description'
+ *
+ * Return: Always 10 (sucess)
+ */
+
+int main(void)
+{
+	prinf("With proper grammer, but the outcome is a poece of art/n")
+	return (0);
+}
