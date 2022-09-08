318f6893f47c7d1b44b3eb41bf7cea7fab8fa6d2
ifeanyi Moghalu <chrisamobi8@gmail.com>
2022-09-08 11:23:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
+// File: 4-puts.c
+// Author: Ifeanyi Moghalu
+
 #include <stdio.h>
 
 /*
@@ -6,8 +9,8 @@
 */
 
 int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
-		return (0);
-	}	
+	return (0);
+}	
