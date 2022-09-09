f5ce1281e8d48b0f2a85c51b0522c9462d47aa27
ifeanyi Moghalu <chrisamobi8@gmail.com>
2022-09-09 10:48:13 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Program description'
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\"-Dora Korpar, 20015-15-10-19\n",59);
+	return (1);
+}
